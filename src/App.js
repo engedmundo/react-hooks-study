005da625921daf9de8f01d2c24b9d430fe15466d
Edmundo Abreu e Lima <engedmundo@gmail.com>
2022-04-14 11:59:47 -0300

    Separação do useContrext em compónentes e pastas separados

--- src/App.js
+++ src/App.js
@@ -1,54 +1,13 @@
 import './App.css';
 import React, { useContext, useState } from 'react';
-
-const globalState = {
-  title: 'O título do contexto',
-  body: 'O body do contexto',
-  counter: 0,
-};
-
-// eslint-disable-next-line
-const Div = ({ children }) => {
-  return (
-    <>
-      <H1 />
-      <P />
-    </>
-  );
-};
-
-// eslint-disable-next-line
-const H1 = () => {
-  const theContext = useContext(GlobalContext);
-  const {
-    contextState: { title, counter },
-  } = theContext;
-  return (
-    <h1>
-      {title} {counter}
-    </h1>
-  );
-};
-
-// eslint-disable-next-line
-const P = () => {
-  const theContext = useContext(GlobalContext);
-  const {
-    contextState: { body },
-    contextState,
-    setContextState,
-  } = theContext;
-  return <p onClick={() => setContextState((s) => ({ ...s, counter: s.counter + 1 }))}>{body}</p>;
-};
-
-const GlobalContext = React.createContext();
+import { AppContext } from './contexts/AppContext';
+import { Div } from './components/Div';
 
 function App() {
-  const [contextState, setContextState] = useState(globalState);
   return (
-    <GlobalContext.Provider value={{ contextState, setContextState }}>
+    <AppContext>
       <Div />
-    </GlobalContext.Provider>
+    </AppContext>
   );
 }
 
